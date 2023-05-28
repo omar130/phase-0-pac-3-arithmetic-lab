0fd071d8b393eab5cd4aed3b5028a8c8c1df3796
Omar Richards <omarrichards01@gmail.com>
2023-05-28 14:17:55 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(){
+  // we'll fill this shortly
+}
+function subtract(){
+  //we'll fill this shortly
+}
+function multiply(){
+  //we'll fill this shortly
+}
+function divide(){
+  //we'll fill this shortly
+}
+
+function add(a, b){
+  return a + b;
+}
+function subtract(a, b){
+  return a - b;
+}
+function multiply(a, b){
+  return a * b;
+}
+function divide(a, b){
+  return a / b;
+}
+function increment(n){
+  n++;
+  return n;
+}
+function decrement(n) {
+  n--;
+  return n;
+}
+function makeInt(n){
+  parseInt(n, 10);
+  return "n";
+
+
+}
+function preserveDecimal(n){
+  parseFloat(n);
+  return "n";
+ 
+}
+
